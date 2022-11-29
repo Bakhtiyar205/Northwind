bb0002aa646183c09c4584c1d7b8539746581645
Bakhtiyar Amrahov <bakhtiyaria@code.edu.az>
2022-11-30 00:17:58 +0400

    BusinessCoreDataAccessEntitiesWebApiInitial

---
+++ DataAccess/Concrete/EntityFramwork/Contexts/NorthwindContext.cs
@@ -0,0 +1,20 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.EntityFramwork.Contexts
+{
+    public class NorthwindContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString: @"Server=DESKTOP-FARR8IG\SQLEXPRESS;Database=Northwind;Trusted_Connection=true");
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
